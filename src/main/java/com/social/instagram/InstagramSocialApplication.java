cf9f9c30463edcf5751025d30199faae6068947d
choitaehoon <xognsdl1230@naver.com>
2020-10-19 19:10:35 +0900

    [#10] session 작업
    - 주석 일관성 있게 변경

--- src/main/java/com/social/instagram/InstagramSocialApplication.java
+++ src/main/java/com/social/instagram/InstagramSocialApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /*
     @EnableAspectJAutoProxy
-    @Aspect를 제공해주고 스프링 XML 설정의 <aop:aspectj-autoproxy/>을 대신해서 자바에 설정합니다.
+    @Aspect를 제공해주고 스프링 XML 설정의 <aop:aspectj-autoproxy/>을 대신해서 자바에 설정한다.
 */
 @SpringBootApplication
 @EnableAspectJAutoProxy
