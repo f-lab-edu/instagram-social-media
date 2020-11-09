9e0b763fe013c1cbb3d381fd3f41f5ce7e5cb2ca
choitaehoon <xognsdl1230@naver.com>
2020-11-09 15:51:44 +0900

    [#16] AWS S3 이미지 업로드
    - 디버깅에 용이하게 만들고자 ExceptionHandler안에 stacktrace 출력 구현

--- src/main/java/com/social/instagram/exception/ApiExceptionHandler.java
+++ src/main/java/com/social/instagram/exception/ApiExceptionHandler.java
@@ -2,6 +2,7 @@ package com.social.instagram.exception;
 
 import com.social.instagram.validation.ErrorMessage;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -27,32 +28,32 @@ public class ApiExceptionHandler {
     */
     @ExceptionHandler(UserIdDuplicatedException.class)
     public ResponseEntity<String> handleDuplicateUserIdException(UserIdDuplicatedException exception) {
-        log.error("[{}] 중복된 아이디가 존재합니다.", exception.toString());
+        log.error("[{}] {}", "중복된 아이디가 존재합니다.", ExceptionUtils.getStackTrace(exception));
         return RESPONSE_USER_ID_BAD_REQUEST;
     }
 
     @ExceptionHandler(UserNotAccountException.class)
     public ResponseEntity<String> handleNotUserDataException(UserNotAccountException exception) {
-        log.error("[{}] 입력된 이메일과 일치하는 계정이 없습니다", exception.toString());
+        log.error("[{}] {}", "입력된 이메일과 일치하는 계정이 없습니다" ,ExceptionUtils.getStackTrace(exception));
         return RESPONSE_USER_ACCOUNT_BAD_REQUEST;
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
         ErrorMessage errorMessage = new ErrorMessage(exception.getBindingResult().getFieldErrors());
-        log.error("[{}] {}", exception.toString(), errorMessage.getErrorMessage());
+        log.error("[{}] {}", errorMessage.getErrorMessage(), ExceptionUtils.getStackTrace(exception));
         return new ResponseEntity<>(errorMessage.getErrorMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(UserNotLoginException.class)
     public ResponseEntity<String> handleUserNotLoginException(UserNotLoginException exception) {
-        log.error("[{}] 유저에 대한 정보를 찾지 못했습니다. 로그인을 해주세요", exception.toString());
+        log.error("[{}] {}", "유저에 대한 정보를 찾지 못했습니다. 로그인을 해주세요" ,ExceptionUtils.getStackTrace(exception));
         return RESPONSE_USER_UNAUTHORIZED;
     }
 
     @ExceptionHandler(AwsS3FileNotUploadException.class)
     public ResponseEntity<String> handleAwsS3FileNotUploadException(AwsS3FileNotUploadException exception) {
-        log.error("[{}] s3 파일 업로드가 실패 했습니다", exception.toString());
+        log.error("[{}] {}", "s3 파일 업로드가 실패 했습니다", ExceptionUtils.getStackTrace(exception));
         return RESPONSE_AWS_S3_FILE_NOT_UPLOAD;
     }
 
