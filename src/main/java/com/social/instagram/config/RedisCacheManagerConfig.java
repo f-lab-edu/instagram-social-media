c2c7414e6ec0056a40b83acf071d79a88bc63422
choitaehoon <xognsdl1230@naver.com>
2020-11-16 11:57:39 +0900

    [#26] 레디스 CacheManager 설정
    - 세션과 캐쉬 레디스 분리
    - RedisConfig -> RedisSessionConfig로 naming 변경

--- src/main/java/com/social/instagram/config/RedisCacheManagerConfig.java
+++ src/main/java/com/social/instagram/config/RedisCacheManagerConfig.java
@@ -1,12 +1,15 @@
 package com.social.instagram.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -15,12 +18,21 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @RequiredArgsConstructor
 public class RedisCacheManagerConfig {
 
-    private final RedisConnectionFactory redisConnectionFactory;
+    @Value("${spring.redis.cache.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.cache.port}")
+    private int redisPort;
+
+    @Bean
+    public RedisConnectionFactory redisCacheConnectionFactory() {
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(redisHost, redisPort));
+    }
 
     @Bean
     public CacheManager redisCacheManager() {
         return RedisCacheManager.RedisCacheManagerBuilder
-                .fromConnectionFactory(redisConnectionFactory)
+                .fromConnectionFactory(redisCacheConnectionFactory())
                 .cacheDefaults(redisCacheConfiguration())
                 .build();
     }
