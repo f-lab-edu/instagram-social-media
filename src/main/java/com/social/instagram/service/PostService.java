2121d21322e0c117eef50fc0fd4a4da9e5e9580f
choitaehoon <xognsdl1230@naver.com>
2020-11-30 18:41:28 +0900

    [#40] 카프카 배치 처리
    - KafkaListener 구현 (아직 batch에 대한 이해도가 높지 않아 공부하면서 세밀하게 구현할 예정)

--- src/main/java/com/social/instagram/service/PostService.java
+++ src/main/java/com/social/instagram/service/PostService.java
@@ -5,10 +5,12 @@ import com.social.instagram.dto.PostDto;
 import com.social.instagram.dto.response.PostResponseDto;
 import com.social.instagram.repository.PostNiceRepository;
 import com.social.instagram.repository.PostRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.util.Streamable;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,12 +18,23 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Service
-@RequiredArgsConstructor
 public class PostService {
 
     private final PostRepository postRepository;
     private final SessionService sessionService;
     private final PostNiceRepository postNiceRepository;
+    private final KafkaTemplate<String, Long> kafkaTemplate;
+    private final String niceTopic;
+
+    public PostService(final PostRepository postRepository, final SessionService sessionService,
+                       final PostNiceRepository postNiceRepository, final KafkaTemplate<String, Long> kafkaTemplate,
+                       @Value("${kafka.topic.type.nice}") final String niceTopic) {
+        this.postRepository = postRepository;
+        this.sessionService = sessionService;
+        this.postNiceRepository = postNiceRepository;
+        this.kafkaTemplate = kafkaTemplate;
+        this.niceTopic = niceTopic;
+    }
 
     public void writePost(Post post) {
         postRepository.save(post);
@@ -44,7 +57,16 @@ public class PostService {
     }
 
     public void updateNice(long id) {
-        postNiceRepository.updateNice(id);
+        kafkaTemplate.send(niceTopic, id);
+    }
+
+    @KafkaListener(topics = "nice", groupId = "nice",
+            containerFactory = "ListenerContainerFactory")
+    public void receiveNiceMessage(List<Long> niceMessage) {
+        for (long niceId : niceMessage) {
+            postNiceRepository.updateNice(niceId);
+        }
+
     }
 
 }
