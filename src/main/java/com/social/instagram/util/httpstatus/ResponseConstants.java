9a314e9afe0232d8fc61849da6e73ef3e878ac83
choitaehoon <xognsdl1230@naver.com>
2020-12-05 16:00:49 +0900

    [#44] 팔로우 구현
    - HTTP 상태코드 204로 변경

--- src/main/java/com/social/instagram/util/httpstatus/ResponseConstants.java
+++ src/main/java/com/social/instagram/util/httpstatus/ResponseConstants.java
@@ -11,6 +11,9 @@ public class ResponseConstants {
     public static final ResponseEntity<Void> RESPONSE_ENTITY_CREATE =
             ResponseEntity.status(HttpStatus.CREATED).build();
 
+    public static final ResponseEntity<Void> RESPONSE_ENTITY_NO_CONTENT =
+            ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+
     public static final ResponseEntity<String> RESPONSE_USER_ID_BAD_REQUEST =
             ResponseEntity.badRequest().body("중복된 userId 입니다");
 
