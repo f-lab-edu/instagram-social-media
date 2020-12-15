66d96fa35f51d282763c4c425161f8f4a681f092
choitaehoon <xognsdl1230@naver.com>
2020-12-15 19:41:05 +0900

    [#46] 팔로우 취소
    - Transactional rollbackFor 제거

--- src/main/java/com/social/instagram/repository/FollowRepository.java
+++ src/main/java/com/social/instagram/repository/FollowRepository.java
@@ -1,13 +1,12 @@
 package com.social.instagram.repository;
 
 import com.social.instagram.domain.Follow;
-import com.social.instagram.exception.FollowOperationFailedException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 
-    @Transactional(rollbackFor = FollowOperationFailedException.class)
+    @Transactional
     void deleteByUserIdAndFollowId(String userId, String followId);
 
 }
