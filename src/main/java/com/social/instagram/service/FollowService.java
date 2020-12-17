f917b86eeaa175a7a16f1a96aec76b0c36c2f99b
choitaehoon <xognsdl1230@naver.com>
2020-12-17 20:08:18 +0900

    [#50] 팔로우 알람 push
    - 파이어베이스 message 전송

--- src/main/java/com/social/instagram/service/FollowService.java
+++ src/main/java/com/social/instagram/service/FollowService.java
@@ -1,22 +1,49 @@
 package com.social.instagram.service;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
 import com.social.instagram.domain.Follow;
 import com.social.instagram.repository.FollowRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class FollowService {
 
     private final FollowRepository followRepository;
+    private final String firebaseToken;
+    private final String messageTitle;
+    private final String messageBody;
+
+    public FollowService(final FollowRepository followRepository,
+                         @Value("${firebase.token}") final String firebaseToken,
+                         @Value("${firebase.title}") final String messageTitle,
+                         @Value("${firebase.body}") final String messageBody) {
+        this.followRepository = followRepository;
+        this.firebaseToken = firebaseToken;
+        this.messageTitle = messageTitle;
+        this.messageBody = messageBody;
+    }
 
     public void follow(Follow follow) {
         followRepository.save(follow);
+
+        FirebaseMessaging.getInstance().sendAsync(followMessage(follow));
     }
 
     public void cancelFollow(Follow follow) {
         followRepository.deleteByUserIdAndFollowId(follow.getUserId(), follow.getFollowId());
+    }
+
+    private Message followMessage(Follow follow) {
+        return Message.builder()
+                .setToken(firebaseToken)
+                .setNotification(Notification.builder()
+                        .setTitle(messageTitle)
+                        .setBody(follow.getUserId() + messageBody)
+                        .build())
+                .build();
     }
 
 }
