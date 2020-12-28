6cb8e1fd3180259a80b4b03734a01e43fe66ef21
choitaehoon <xognsdl1230@naver.com>
2020-12-28 14:11:29 +0900

    [#55] 피드 댓글 작성
    - import Mockito * 제거

--- src/test/java/com/social/instagram/service/CommentServiceTest.java
+++ src/test/java/com/social/instagram/service/CommentServiceTest.java
@@ -14,7 +14,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class CommentServiceTest {
