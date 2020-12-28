2e9fdb40d4e7bcd1c8759c8faacba23fd08c97f5
choitaehoon <xognsdl1230@naver.com>
2020-12-28 14:07:56 +0900

    [#55] 피드 댓글 작성
    - 댓글 test작성
    - CommentRequestDto builder 적용

--- src/test/java/com/social/instagram/service/CommentServiceTest.java
+++ src/test/java/com/social/instagram/service/CommentServiceTest.java
@@ -1,29 +1,59 @@
 package com.social.instagram.service;
 
+import com.social.instagram.domain.Comment;
 import com.social.instagram.dto.request.CommentRequestDto;
 import com.social.instagram.repository.CommentRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-/*
-    작성해야 할 테스트 목록
-    1. postId가 음수이거나 userId가 없다면 실패하기 -> 전반적인 comment 유효성 검사
-    2. 댓글을 작성하면 테스트 성공
-*/
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class CommentServiceTest {
 
     @Mock
     private CommentRepository commentRepository;
+
+    @Mock
+    private SessionService sessionService;
+
+    @Mock
+    private Comment comment;
 
     @InjectMocks
     private CommentService commentService;
 
     private CommentRequestDto commentRequestDto;
 
-    @Test
-    public void comment_Fail_When_No_PostIdOrUserId() {
+    private static final long POST_ID = 84L;
+    private static final String COMMENT = "댓글 작성";
+    private static final String USER_ID = "testId";
 
+    @BeforeEach
+    public void setUp() {
+        this.commentRequestDto = CommentRequestDto.builder()
+                .postId(POST_ID)
+                .comment(COMMENT)
+                .build();
+    }
+
+    @Test
+    public void commentSuccessSave() {
+        given(sessionService.getUserId()).willReturn(USER_ID);
+        given(commentRepository.save(any())).willReturn(comment);
+
+        commentService.comment(commentRequestDto);
+
+        assertThat(sessionService.getUserId(), equalTo(USER_ID));
+        verify(commentRepository).save(any());
     }
 
 }
