b58035d590b7b65d0ab29973c84194d3e47e4509
choitaehoon <xognsdl1230@naver.com>
2020-12-31 16:24:47 +0900

    [#57] Aws 사진 저장 테스트코드
    - aws 사진 저장 테스트코드 완료

--- src/test/java/com/social/instagram/service/AwsS3LocalServiceTest.java
+++ src/test/java/com/social/instagram/service/AwsS3LocalServiceTest.java
@@ -12,7 +12,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.net.URL;
+
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -29,25 +32,26 @@ class AwsS3LocalServiceTest {
     private MultipartFile file;
 
     private static final String NAME = "picture";
-    private static final String ORIGINAL_FILE_NAME = "picture";
+    private static final String ORIGINAL_FILE_NAME = "picture.jpeg";
     private static final String CONTENT_TYPE = "image/jpeg";
+    private static final byte[] CONTENT_STREAM = ORIGINAL_FILE_NAME.getBytes();
     private static final String USER_ID = "testId";
 
     @BeforeEach
     public void setUp() {
-        //todo MockMultipartFile에 맨끝에 contentStream 넣기
-        this.file = new MockMultipartFile(NAME, ORIGINAL_FILE_NAME, CONTENT_TYPE);
+        this.file = new MockMultipartFile(NAME, ORIGINAL_FILE_NAME, CONTENT_TYPE, CONTENT_STREAM);
     }
 
     @Test
     @DisplayName("사진을 S3에 저장하고 해당 사진 URL을 반환한다")
     public void getPictureUrlAfterS3Save() {
         given(amazonS3Client.putObject(any())).willReturn(mock(PutObjectResult.class));
+        given(amazonS3Client.getUrl(anyString(), anyString())).willReturn(mock(URL.class));
 
         awsS3LocalService.upload(file, USER_ID);
 
-        //todo url값 검증 하기 assertThat();
         verify(amazonS3Client).putObject(any());
+        verify(amazonS3Client).getUrl(anyString(), anyString());
     }
 
 }
