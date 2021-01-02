bd86aa14c7532dfc49a8a4bffee644339342eec6
choitaehoon <xognsdl1230@naver.com>
2021-01-02 16:32:04 +0900

    [#55] 피드 댓글 작성
    - 댓글 저장 시나리오 자세하게 적기

--- src/test/java/com/social/instagram/service/CommentServiceTest.java
+++ src/test/java/com/social/instagram/service/CommentServiceTest.java
@@ -45,7 +45,7 @@ class CommentServiceTest {
     }
 
     @Test
-    @DisplayName("댓글을 데이터베이스에 저장한다")
+    @DisplayName("레디스에 ID를 가져오고 댓글DTO를 도메인으로 변경 후 댓글 저장에 성공한다")
     public void commentSuccessSave() {
         given(sessionService.getUserId()).willReturn(USER_ID);
         given(commentRepository.save(any())).willReturn(comment);
