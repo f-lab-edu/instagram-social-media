edf3a0890d9b05b4ea4cc37fb500aa8be902ec6d
taehoon <33123391+choitaehoon@users.noreply.github.com>
2021-02-12 00:58:14 +0900

    [#79] 좋아요 Redis 설정 (#80)
    - 좋아요에 대한 Redis 설정
    - RedisCacheManagerConfig파일 삭제

---
+++ src/main/java/com/social/instagram/config/redis/FeedsNiceRedisConfig.java
@@ -0,0 +1,35 @@
+package com.social.instagram.config.redis;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+
+@Configuration
+public class FeedsNiceRedisConfig {
+
+    @Value("${spring.redis.feeds-nice.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.feeds-nice.port}")
+    private int redisPort;
+
+    @Bean
+    public RedisConnectionFactory feedsNiceRedisConnectionFactory() {
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(redisHost, redisPort));
+    }
+
+    @Bean
+    public RedisTemplate<Integer, Integer> feedsNiceRedisTemplate() {
+        RedisTemplate<Integer, Integer> feedsNiceRedisTemplate = new RedisTemplate<>();
+        feedsNiceRedisTemplate.setConnectionFactory(feedsNiceRedisConnectionFactory());
+        feedsNiceRedisTemplate.setKeySerializer(new GenericJackson2JsonRedisSerializer());
+        feedsNiceRedisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return feedsNiceRedisTemplate;
+    }
+
+}
