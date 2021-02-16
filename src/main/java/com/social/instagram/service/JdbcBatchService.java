5ecb597288a7be0522b381054ddb2f0e45799953
choitaehoon <xognsdl1230@naver.com>
2021-02-16 22:38:15 +0900

    [#87] JDBC BatchUpdate 구현하기
    - 배치 주의사항 주석 작성

--- src/main/java/com/social/instagram/service/JdbcBatchService.java
+++ src/main/java/com/social/instagram/service/JdbcBatchService.java
@@ -13,6 +13,16 @@ public class JdbcBatchService {
 
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
+    /*
+        NamedParameterJdbcTemplate의 batchUpdate 메소드는 PreparedStatement에서 값을 자동으로 설정하는데
+        일반적으로 잘 작동하지만 Map에 포함된 Null값일 경우 문제가 발생할 수 있음
+
+        ParameterMetaData.getParameterType은 위의 동작 방식일 경우 호출하게 되는데 JDBC 드라이버에 비용이 많이 들어
+        성능 저하가 발생할 수 있음 (Oracle 12c, PostgreSQL에 보고 됨)
+
+        이럴 경우 spring.jdbc.getParameterType.ignore 속성을 true로 주거나 spring.properties 클래스 경로의 루트 파일로 설정
+        하는 것들을 고려해야함
+    */
     public void batchInsert(final String query, final List<?> batchMessage) {
         namedParameterJdbcTemplate.batchUpdate(query, SqlParameterSourceUtils.createBatch(batchMessage.toArray()));
     }
